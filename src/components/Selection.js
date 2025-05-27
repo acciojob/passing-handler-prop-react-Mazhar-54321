44587004aa2dec4ed3ea5d7beaad86b6f5043f1c
Mazhar-54321 <syedmazharali742@gmail.com>
2025-05-27 13:20:00 +0530

    refactored code

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -7,12 +7,8 @@ import '../styles/App.css';
 const title = 'Select the gradient and then the Box to change the color';
 
 const Selection = ({data,applyColor}) => {
-  const [background,setBackground] = useState('')
-  useEffect(()=>{
-  console.log(background,";;;;")
-  },[background])
   return (
-    <div onClick={()=>applyColor(setBackground)} style={{width:'100px',height:'100px',backgroundColor:background,textAlign:'center',border:'1px solid white'}} >
+    <div  style={{width:'100px',height:'100px',backgroundColor:applyColor,textAlign:'center',border:'1px solid white'}} >
      {data}
     </div >
   )
