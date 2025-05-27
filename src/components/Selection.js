c36cf3820fcfd5fd7592b42486f35e0a16a88c93
Mazhar-54321 <syedmazharali742@gmail.com>
2025-05-27 13:23:07 +0530

    refactored

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -8,7 +8,7 @@ const title = 'Select the gradient and then the Box to change the color';
 
 const Selection = ({data,applyColor}) => {
   return (
-    <div  style={{width:'100px',height:'100px',backgroundColor:applyColor,textAlign:'center',border:'1px solid white'}} >
+    <div data-test-id={applyColor}  style={{width:'100px',height:'100px',backgroundColor:applyColor,textAlign:'center',border:'1px solid white'}} >
      {data}
     </div >
   )
