b17523288da255f14e299dd3adfde765e9dddca9
Mazhar-54321 <syedmazharali742@gmail.com>
2025-05-27 13:42:02 +0530

    refactored

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ const App = () => {
       <div className='row' id="children-wrapper">
         {
           ["selection1", "selection2", "selection3"]?.map(key => (
-            <Selection key={key} data={key} applyColor={nextBackground} />
+            <Selection key={key} data={key} applyColor={applyColor} />
           ))
         }
       </div>
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -7,9 +7,10 @@ import '../styles/App.css';
 const title = 'Select the gradient and then the Box to change the color';
 
 const Selection = ({data,applyColor}) => {
-  console.log(applyColor)
+  const [background,setBackground]= useState({background:'',colorName:''});
+  console.log(background)
   return (
-    <div data-testid={applyColor.colorName}  style={{width:'100px',height:'100px',backgroundColor:applyColor.background,textAlign:'center',border:'1px solid white'}} >
+    <div onClick={()=>applyColor(setBackground)} data-testid={background.colorName}  style={{width:'100px',height:'100px',backgroundColor:background.background,textAlign:'center',border:'1px solid white'}} >
      {data}
     </div >
   )
